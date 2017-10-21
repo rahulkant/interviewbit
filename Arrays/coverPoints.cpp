959a2a2de4f5e6cdc07e522d827e69de2b07d4e8
rahulkant <rahulkant333@gmail.com>
2017-10-21 21:56:44 +0530

    DirectI

--- Arrays/coverPoints.cpp
+++ Arrays/coverPoints.cpp
@@ -1,14 +1,12 @@
-// Input : X and Y co-ordinates of the points in order. 
-// Each point is represented by (X[i], Y[i])
 int Solution::coverPoints(vector<int> &X, vector<int> &Y) {
     int count = 0;
     int diff1, diff2 = 0;
-    if(X.size()==1)
+    if(X.size() == 1)
         return 0;
     for(int i=0; i<X.size()-1; i++)
     {
-        diff1 = abs(X[i+1]-X[i]);
-        diff2 = abs(Y[i+1]-Y[i]);
+        diff1 = abs(X[i+1] - X[i]);
+        diff2 = abs(Y[i+1] - Y[i]);
         if(diff1 > diff2)
             count += diff1;
         else
